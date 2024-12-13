844f53a43a6e03d0c386ceee25cc291da2840bd6
Newton McCollum <newtonmccollum@apptronik.com>
2024-12-13 15:28:33 -0600

    initial config

---
+++ init.lua
@@ -0,0 +1,34 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+vim.g.health_check = 0
+
+vim.wo.number = true
+
+vim.filetype.add {
+  extension = {
+    zsh = "sh",
+    sh = "sh", -- force sh-files with zsh-shebang to still get sh as filetype
+  },
+  filename = {
+    [".zshrc"] = "sh",
+    [".zshenv"] = "sh",
+  },
+}
+
+require "config.lazy"
+
+-- nvim tree
+require("nvim-tree").setup {
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+}
